a54ba800f2e8bad20dc2489ebdc71f2b1289404f
gk <gyanendra.kumar.che21@itbhu.ac.in>
2024-09-22 23:15:26 +0530

    git commit "successful"

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -12,6 +12,7 @@ import axios from "axios";
 // Create Axios instance with dynamic baseURL
 const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api", // Using 'import.meta.env.MODE' for mode detection
+    // baseURL: import.meta.env.MODE === "development" ? "https://full-stack-ecommerce-8cqq.onrender.com/api" : "/api", // Using 'import.meta.env.MODE' for mode detection
     withCredentials: true, // send cookies to the server
 });
  
