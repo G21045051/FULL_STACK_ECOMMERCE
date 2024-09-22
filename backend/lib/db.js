0466ded1a9c69babf9a673f58d95ec3a679b42fc
gk <gyanendra.kumar.che21@itbhu.ac.in>
2024-09-22 23:49:13 +0530

    git commit "done"

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -3,17 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();   
 
-// export const connectDB = async () => {
-// 	try {
-// 		const conn = await mongoose.connect(process.env.MONGO_URL);
-// 		// const conn = await mongoose.connect("mongodb+srv://rishikashakya235:gB4NGzFB25FBvmPL@ecommerce-app.cjy7x.mongodb.net/Ecommerce_db?retryWrites=true&w=majority&appName=Ecommerce-App");
-
-// 		console.log(`MongoDB connected: ${conn.connection.host}`);
-// 	} catch (error) {
-// 		console.log("Error connecting to MONGODB", error.message);
-// 		process.exit(1);
-// 	}
-// };
 
 // this is running fine 
 export const connectDB = async () => {
